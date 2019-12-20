3ea789bca183a7aa38cf02b8c47ed50213bfcd4b
Justin <u06530015@ccu.edu.tw>
2019-12-20 17:17:19 +0800

    新增註冊事件 輸入原則 防止預測字元 鎖定登入畫面下面兩行的相對位置

--- app/src/main/java/com/ccumis/food/MainActivity.java
+++ app/src/main/java/com/ccumis/food/MainActivity.java
@@ -3,6 +3,12 @@ package com.ccumis.food;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.DigitsKeyListener;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //註冊按鍵事件
+    public void regist(View view) {
 
     }
 }
