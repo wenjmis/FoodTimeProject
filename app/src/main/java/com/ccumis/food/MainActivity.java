6cfb3663915d4d034863a120df0c03a5438408f4
JustibShih <love54025422@gmail.com>
2020-03-31 16:39:24 +0800

    新增註冊畫面並且匯入Material Design元件

--- app/src/main/java/com/ccumis/food/MainActivity.java
+++ app/src/main/java/com/ccumis/food/MainActivity.java
@@ -2,9 +2,12 @@ package com.ccumis.food;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.DigitsKeyListener;
 import android.view.KeyEvent;
+import android.view.View;
+import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     //註冊按鍵事件
     public void regist(View view) {
-
+        Intent intent = new Intent(this,registered_page.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/ccumis/food/registered_page.java
@@ -0,0 +1,16 @@
+package com.ccumis.food;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.os.Bundle;
+
+public class registered_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registered_page);
+    }
+}
