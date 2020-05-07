7797e2438f4a973ca2203b9e069573282df4337d
JustibShih <love54025422@gmail.com>
2020-05-08 01:41:30 +0800

    google_map_api 建置事前工作

--- app/src/main/java/com/ccumis/food/home_page.java
+++ app/src/main/java/com/ccumis/food/home_page.java
@@ -14,12 +14,20 @@ import com.ccumis.food.fragment.Page1;
 import com.ccumis.food.fragment.Page2;
 import com.ccumis.food.fragment.Page3;
 import com.ccumis.food.fragment.Page4;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
-public class home_page extends AppCompatActivity {
+public class home_page extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
 
     @Override
@@ -42,6 +50,9 @@ public class home_page extends AppCompatActivity {
         tabLayout.getTabAt(2).setIcon(getResources().getDrawable(R.drawable.ic_forum_24px));
         tabLayout.getTabAt(3).setIcon(getResources().getDrawable(R.drawable.ic_settings_applications_24px));
 
+        //google_map
+        // mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        //mapFragment.getMapAsync(this);
     }
 
 
@@ -58,6 +69,16 @@ public class home_page extends AppCompatActivity {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng sydney = new LatLng(-34,151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
     class ViewPagerAdapter extends FragmentPagerAdapter{
 
         private ArrayList<Fragment> fragments;
