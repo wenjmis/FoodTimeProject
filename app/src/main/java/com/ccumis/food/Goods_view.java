724b2407518b852494df47015f7f1ce2460ba201
JustibShih <love54025422@gmail.com>
2020-05-11 01:06:07 +0800

    新增商品及顯示商品

---
+++ app/src/main/java/com/ccumis/food/Goods_view.java
@@ -0,0 +1,80 @@
+package com.ccumis.food;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ccumis.food.Model.commodity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Goods_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goods_view);
+        Button button = findViewById(R.id.button4);
+        final TextView good_name =findViewById(R.id.good_name);
+        final TextView last_time = findViewById(R.id.last_time);
+        final TextView username = findViewById(R.id.username);
+        final TextView price = findViewById(R.id.price);
+        final TextView good_distribution= findViewById(R.id.address);
+        final TextView address= findViewById(R.id.address);
+
+
+
+
+        Intent intent = getIntent();
+        Bundle get = intent.getExtras();
+
+        final String receiver_id = get.getString("receiver_id");
+        final String goodname = get.getString("good_name");
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Intent intent =  new Intent(Goods_view.this, chatroom.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("receiver_id",receiver_id);
+                intent.putExtras(bundle);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        DatabaseReference reference  = FirebaseDatabase.getInstance().getReference("commodity");
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    commodity commodity = snapshot.getValue(commodity.class);
+                    if(commodity.good_name.equals(goodname)){
+                        good_name.setText(commodity.good_name);
+                        last_time.setText("剩餘時間:"+commodity.last_time);
+                        username.setText("上傳者"+commodity.user_id);
+                        price.setText("費用:"+commodity.good_price);
+                        good_distribution.setText("簡介:\n"+commodity.good_distribution);
+                        address.setText("地址:\n"+commodity.address);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/ccumis/food/addGoods.java
+++ app/src/main/java/com/ccumis/food/addGoods.java
@@ -1,7 +1,14 @@
 package com.ccumis.food;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
@@ -11,6 +18,17 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 public class addGoods extends AppCompatActivity {
 
@@ -22,7 +40,7 @@ public class addGoods extends AppCompatActivity {
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int width = displayMetrics.widthPixels;
         int height = displayMetrics.heightPixels;
-        getWindow().setLayout(width*1,(int)(height*.9));
+        getWindow().setLayout(width * 1, (int) (height * .9));
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.gravity = Gravity.BOTTOM;
         params.x = 0;
@@ -37,15 +55,14 @@ public class addGoods extends AppCompatActivity {
                 finish();
             }
         });
-        final Switch my_switch  = findViewById(R.id.chip2);
-        final EditText editText = findViewById(R.id.editText13);
+        final Switch my_switch = findViewById(R.id.chip2);
+        final EditText editText = findViewById(R.id.price);
         my_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(my_switch.isChecked()){
+                if (my_switch.isChecked()) {
                     editText.setEnabled(true);
-                }
-                else
+                } else
                     editText.setEnabled(false);
             }
         });
@@ -53,8 +70,53 @@ public class addGoods extends AppCompatActivity {
 
     }
 
+    public void submit_goods(View view) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference("commodity");
+
+        Locations locations = new Locations();
+        EditText good_distribution=findViewById(R.id.good_distribution);
+        EditText good_name=findViewById(R.id.good_name);
+        EditText good_price=findViewById(R.id.price);
+        EditText last_time=findViewById(R.id.last_time);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM月dd日HH:mm");
+        Date curDate = new Date(System.currentTimeMillis());
+
+        HashMap<String,Object> hashMap = new HashMap<>();
+        hashMap.put("address",locations.locationlist);
+        hashMap.put("good_distribution",good_distribution.getText().toString());
+        hashMap.put("good_name",good_name.getText().toString());
+        hashMap.put("good_price",good_price.getText().toString());
+        hashMap.put("last_time",last_time.getText().toString());
+        hashMap.put("start_time",simpleDateFormat.format(curDate));
+        hashMap.put("telephone","");
+        hashMap.put("user_id",user.getUid());
+        database.setValue(hashMap);
+    }
     @Override
     public void onBackPressed() {
         finish();
+    }
+    class Locations implements LocationListener {
+        String locationlist;
+        @Override
+        public void onLocationChanged(Location location) {
+            locationlist = location.getLatitude()+","+location.getLongitude();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
     }
 }
